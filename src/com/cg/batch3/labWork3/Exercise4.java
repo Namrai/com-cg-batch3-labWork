9e365eb657158fe9536d886db87dcf5fedf896d0
Namrai <namratarai1599@gmail.com>
2021-10-28 16:41:57 +0530

    Update Exercise4.java
    commit

--- src/com/cg/batch3/labWork3/Exercise4.java
+++ src/com/cg/batch3/labWork3/Exercise4.java
@@ -1,15 +1,41 @@
 package com.cg.batch3.labWork3;
 
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.*;
+
+//to display number of line char and text.
 
 public class Exercise4 {
-	public static void main(String[] args) {
-		System.out.println("enter the interger value");
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		String str= String.valueOf(num);
-		for(int i=0;i<str.length();i++){
-		    char a=str.charAt(i);
-}
-}
+	public static void main(String[] args) 
+	{
+		try {
+		int noChar=0;
+		int noline=0;
+		int noWord=0;
+		int c=0;
+	
+		FileInputStream func =new FileInputStream("D:\\sample.txt");
+		while((func.available()!=0))
+		{ 
+			c=func.read();
+			if(c!=10)
+				noChar++;
+			if(c == 32)
+				noWord++;
+			if(c==13)
+			{
+				noline++;
+				noWord++;
+			}
+		}
+		System.out.println(noChar);
+		System.out.println(noWord);
+		System.out.println(noline);
+		func.close();
+		}catch(FileNotFoundException e) {
+			System.out.println("Cannot find the specified  file ");
+		}catch(IOException i) {
+			System.out.println("cannot read file");
+		}
+	}
 }
